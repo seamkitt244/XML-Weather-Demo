264f5b9148d84c6153f8644aa0a78ff0890d6b9e
seamus kittmer <seamuskittmer@gmail.com>
2020-04-24 15:31:33 -0400

    XMLWeatherdemo

---
+++ XMLWeather/Search.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace XMLWeather
+{
+    partial class Search
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Search));
+            this.label1 = new System.Windows.Forms.Label();
+            this.forecastLabel = new System.Windows.Forms.Label();
+            this.todayLabel = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.searchTextBox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.city1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Yu Gothic UI", 18.125F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(529, 39);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(177, 66);
+            this.label1.TabIndex = 79;
+            this.label1.Text = "Search";
+            // 
+            // forecastLabel
+            // 
+            this.forecastLabel.BackColor = System.Drawing.Color.Transparent;
+            this.forecastLabel.Font = new System.Drawing.Font("Yu Gothic UI", 18.125F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.forecastLabel.ForeColor = System.Drawing.Color.White;
+            this.forecastLabel.Location = new System.Drawing.Point(293, 39);
+            this.forecastLabel.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.forecastLabel.Name = "forecastLabel";
+            this.forecastLabel.Size = new System.Drawing.Size(165, 53);
+            this.forecastLabel.TabIndex = 77;
+            this.forecastLabel.Text = "7 Day";
+            this.forecastLabel.Click += new System.EventHandler(this.forecastLabel_Click);
+            // 
+            // todayLabel
+            // 
+            this.todayLabel.BackColor = System.Drawing.Color.Transparent;
+            this.todayLabel.Font = new System.Drawing.Font("Yu Gothic UI", 18.125F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.todayLabel.ForeColor = System.Drawing.Color.White;
+            this.todayLabel.Location = new System.Drawing.Point(53, 39);
+            this.todayLabel.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.todayLabel.Name = "todayLabel";
+            this.todayLabel.Size = new System.Drawing.Size(165, 53);
+            this.todayLabel.TabIndex = 76;
+            this.todayLabel.Text = "Today";
+            this.todayLabel.Click += new System.EventHandler(this.todayLabel_Click);
+            // 
+            // label5
+            // 
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(489, 54);
+            this.label5.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(230, 52);
+            this.label5.TabIndex = 78;
+            this.label5.Text = "____________________________";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // searchTextBox
+            // 
+            this.searchTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.875F);
+            this.searchTextBox.Location = new System.Drawing.Point(62, 154);
+            this.searchTextBox.Multiline = true;
+            this.searchTextBox.Name = "searchTextBox";
+            this.searchTextBox.Size = new System.Drawing.Size(512, 60);
+            this.searchTextBox.TabIndex = 80;
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button1.Location = new System.Drawing.Point(601, 154);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 60);
+            this.button1.TabIndex = 81;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(628, 768);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 25);
+            this.label3.TabIndex = 83;
+            this.label3.Text = "label3";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(0, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 25);
+            this.label2.TabIndex = 84;
+            this.label2.Text = "label2";
+            // 
+            // city1
+            // 
+            this.city1.AutoSize = true;
+            this.city1.Font = new System.Drawing.Font("Yu Gothic UI", 12.125F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.city1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.city1.Location = new System.Drawing.Point(59, 256);
+            this.city1.Name = "city1";
+            this.city1.Size = new System.Drawing.Size(96, 45);
+            this.city1.TabIndex = 85;
+            this.city1.Text = "city 1";
+            // 
+            // Search
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Controls.Add(this.city1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.searchTextBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.forecastLabel);
+            this.Controls.Add(this.todayLabel);
+            this.Controls.Add(this.label5);
+            this.Name = "Search";
+            this.Size = new System.Drawing.Size(750, 1026);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label forecastLabel;
+        private System.Windows.Forms.Label todayLabel;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox searchTextBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label city1;
+    }
+}
---
+++ XMLWeather/Search.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using System.Net;
+using System.Windows.Forms;
+
+namespace XMLWeather
+{
+    public partial class Search : UserControl
+    {
+        public static Boolean search;
+        public static string city;
+        List<string> cities = new List<string>();
+        public Search()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            city = searchTextBox.Text;
+            search = true;
+            Form1.urlCur = "http://api.openweathermap.org/data/2.5/weather?q=" + city + ",CA&mode=xml&units=metric&appid=3f2e224b815c0ed45524322e145149f0";
+            Form1.urlFore = "http://api.openweathermap.org/data/2.5/forecast/daily?q=" + city + ",CA&mode=xml&units=metric&cnt=7&appid=3f2e224b815c0ed45524322e145149f0";
+            Form1.days.Clear();
+            Form f = this.FindForm();
+            f.Controls.Remove(this);
+            Form1 NewForm = new Form1();
+            NewForm.Show();
+            this.Dispose(false);
+            //CurrentScreen cs = new CurrentScreen();
+            //f.Controls.Add(cs);
+        }
+        #region Top label clicks
+        private void forecastLabel_Click(object sender, EventArgs e)
+        {
+            Form f = this.FindForm();
+            f.Controls.Remove(this);
+            ForecastScreen fs = new ForecastScreen();
+            f.Controls.Add(fs);
+        }
+        private void todayLabel_Click(object sender, EventArgs e)
+        {
+            Form f = this.FindForm();
+            f.Controls.Remove(this);
+            CurrentScreen cs = new CurrentScreen();
+            f.Controls.Add(cs);
+        }
+        #endregion
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
